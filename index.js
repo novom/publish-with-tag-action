c5b78fbf7cbffb0238d7c5edc3de67a9b3d51687
Philippe Durocher <pdurocher@novominteractive.com>
2020-02-14 14:55:23 -0500

    move entire action inside function

--- index.js
+++ index.js
@@ -2,32 +2,36 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 
-try {
-  const versionTag = github.context.ref.split('/')[2].split('-');
-  let args = ['publish'];
-  let tag = 'latest';
-  const options = {
-    listeners: {
-      stdout: (data) => {
-        console.log(data.toString());
+async function publish() {
+  try {
+    const versionTag = github.context.ref.split('/')[2].split('-');
+    let args = ['publish'];
+    let tag = 'latest';
+    const options = {
+      listeners: {
+        stdout: (data) => {
+          console.log(data.toString());
+        },
+        stderr: (data) => {
+          throw new Error(data.toString());
+        },
       },
-      stderr: (data) => {
-        throw new Error(data.toString());
-      },
-    },
-  };
+    };
 
-  if (versionTag.length === 1) {
-    console.log('Release is stable');
-  } else {
-    console.log('Release is a pre-release');
-    tag = versionTag[1].split('.')[0];
-    args.push('--tag=' + tag);
+    if (versionTag.length === 1) {
+      console.log('Release is stable');
+    } else {
+      console.log('Release is a pre-release');
+      tag = versionTag[1].split('.')[0];
+      args.push('--tag=' + tag);
+    }
+
+    await exec.exec('npm', args, options);
+    core.setOutput('version', versionTag.join('-'));
+    core.setOutput('tag', tag);
+  } catch (error) {
+    core.setFailed(error.message);
   }
-
-  await exec.exec('npm', args, options);
-  core.setOutput('version', versionTag.join('-'));
-  core.setOutput('tag', tag);
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+publish();
