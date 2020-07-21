30e9748b5ee5b9136d00c69b152e30a886e897b8
Philippe Durocher <pdurocher@novominteractive.com>
2020-07-21 16:34:34 -0400

    move code in src and add test command

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,10 @@ import json from '@rollup/plugin-json';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
-  input: 'index.js',
+  input: 'src/index.js',
   output: {
     file: 'dist/bundle.js',
-    format: 'cjs'
+    format: 'cjs',
   },
   plugins: [commonjs(), json(), nodeResolve()],
 };
